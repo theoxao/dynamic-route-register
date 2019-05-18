14dcc896bd04a94e9aeec1cbcd9bd60c4938d5fe
theoxao <theoxao@163.com>
2019-05-18 22:27:58 +0800

    feat():dynamic add or delete route

--- src/main/java/com/test/RedisMessageListenerConfig.kt
+++ src/main/java/com/test/RedisMessageListenerConfig.kt
@@ -1,13 +1,16 @@
 package com.test
 
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
+import io.ktor.http.HttpMethod
 import io.ktor.response.respond
-import io.ktor.routing.Routing
+import io.ktor.routing.*
 import io.ktor.routing.get
-import io.ktor.routing.routing
 import io.ktor.server.engine.ApplicationEngine
 import io.ktor.util.AttributeKey
 import io.ktor.util.Attributes
+import io.ktor.util.pipeline.ContextDsl
+import io.ktor.util.pipeline.PipelineInterceptor
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.MessageListener
@@ -15,16 +18,21 @@ import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.listener.PatternTopic
 import org.springframework.data.redis.listener.RedisMessageListenerContainer
 import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor
+import org.w3c.dom.Attr
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.jvm.javaField
 
 
 /**
  * @author theo
  * @date 2019/5/17
  */
-internal val FEATURE_INSTALLED_LIST = AttributeKey<Attributes>("ApplicationFeatureRegistry")
+internal val internalKey = AttributeKey<String>("ID")
 
 @Configuration
 open class RedisMessageListenerConfig(private val redisTemplate: RedisTemplate<Any, Any>, private val applicationEngine: ApplicationEngine) {
+
+
     @Bean
     open fun redisMessageListenerContainer(): RedisMessageListenerContainer {
         val container = RedisMessageListenerContainer()
@@ -34,26 +42,63 @@ open class RedisMessageListenerConfig(private val redisTemplate: RedisTemplate<A
         container.addMessageListener(MessageListener { message, _ ->
             val body = String(message.body)
             val channel = String(message.channel)
+            println("channel -> $channel \tbody -> $body")
             var value: Any? = null
             val attributes = applicationEngine.application.attributes
             val attributeKey = attributes.allKeys.filter { it.name == "ApplicationFeatureRegistry" }[0] as AttributeKey<Attributes>
             val attribute = attributes[attributeKey]
-            val routeKey = attribute.allKeys.filter { it.name == "Routing" }[0]  as AttributeKey<Attributes>
-            val route:Any = attribute[routeKey]
+            val routeKey = attribute.allKeys.filter { it.name == "Routing" }[0] as AttributeKey<Attributes>
+            val route: Any = attribute[routeKey]
             if (route is Routing) {
-                route.children
-            }
-            applicationEngine.application.routing {
-                children.forEach {
-                    it.
+                val childList = route.childList()
+                when {
+                    channel.contains("lrem") -> {
+                        println("removing $body")
+                        childList.forEach {
+                           val grandChildList = it.childList()
+                            grandChildList.removeIf {r->
+                                val key = r.attributes.allKeys.stream().filter { it.name == "ID" }.findAny()
+                                if (key.isPresent) {
+                                    val realKey = key.get() as AttributeKey<String>
+                                    val v = r.attributes.getOrNull(realKey)
+                                    println(v)
+                                    return@removeIf v == body
+                                }
+                                false
+                            }
+
+                        }
+                    }
+                    channel.contains("lpush") ->
+                        applicationEngine.application.routing {
+                            markedRoute(body, HttpMethod.Get, body) {
+                                handle {
+                                    this.context.attributes.put(internalKey, body)
+                                    call.respond("channel -> $channel \tbody -> $body")
+                                }
+                            }
+
+                        }
                 }
-                get(body) {
-                    call.respond("channel -> $channel \tbody -> $body")
-                }
             }
+
         }, topic)
         return container
     }
+}
 
 
+fun Route.childList(): MutableList<Route> {
+    val field = Route::class.declaredMemberProperties.stream().filter { it.name == "childList" }.findAny().get().javaField
+    field?.isAccessible = true
+    return field?.get(this) as MutableList<Route>
+}
+
+@ContextDsl
+fun Route.markedRoute(path: String, method: HttpMethod, value: String, build: Route.() -> Unit): Route {
+    val selector = HttpMethodRouteSelector(method)
+    val createRouteFromPath = createRouteFromPath(path)
+    val child = createRouteFromPath.createChild(selector)
+    child.attributes.put(internalKey, value)
+    return child.apply(build)
 }
